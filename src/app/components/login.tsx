3e6de6ef6d8ae8cb6c461b069602263c2e1e7881
Bhargav Gudapati <bhargav_gudapati@yahoo.com>
2024-11-23 22:04:38 -0500

    working on logout

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -2,6 +2,21 @@
 'use client';
 import React, { useState } from 'react';
 import styles from '../components/login.module.css';
+import Navbar from './navbar';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
+async function doLogout() {
+    const router = useRouter();
+    console.log("logging out");
+    router.push("/login");
+    const x = () => {};
+    await x();;
+}
+
+async function doLogin() {
+    const router = useRouter();
+}
 
 const LoginPage: React.FC = () => {
 
@@ -14,30 +29,29 @@ const LoginPage: React.FC = () => {
     }
 
     return (
-        <div className={styles.loginPage}>
-            <h1>NovelNotes</h1>
-            
-            {/* Container for form fields */}
-            <div className={styles.formContainer}>
-            <form onSubmit={onLoginSubmit}>
-                    <label>Username</label>
-                    <input value={user} onChange={e => setUser(e.target.value)} type="text" placeholder="Enter username" />
-                  
-                    <label>Password</label>
-                    <input type="password" placeholder="Enter password" />
-                    <button type="submit">Sign In</button>
-                </form>
+	<div>
+	    <Navbar />
+            <div className={styles.loginPage}>            
+		<div className={styles.formContainer}>
+		    <form onSubmit={onLoginSubmit}>
+			<label>Username</label>
+			<input value={user} onChange={e => setUser(e.target.value)} type="text" placeholder="Enter username" />                  
+			<label>Password</label>
+			<input type="password" placeholder="Enter password" />
+			<button type="submit">Sign In</button>
+                    </form>
+		</div>
+
+		<p>Your personal library, organized and noted</p>
+
+		<button
+                    className={styles.signUpBtn}
+                    onClick={() => alert("Redirect to Sign Up")}
+		>
+                     Sign Up
+		</button>
             </div>
-
-            <p>Your personal library, organized and noted</p>
-
-            <button
-                className={styles.signUpBtn}
-                onClick={() => alert("Redirect to Sign Up")}
-            >
-                Sign Up
-            </button>
-        </div>
+	</div>
     );
 };
 
