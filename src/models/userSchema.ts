eac498f061d099820dbba21f1510569ea3501255
Bhargav Gudapati <bhargav_gudapati@yahoo.com>
2024-11-24 19:16:18 -0500

    created userSchema

---
+++ src/models/userSchema.ts
@@ -0,0 +1,34 @@
+
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+interface IItem extends Document {
+    firstname: string,
+    username: string,
+    email: string,
+    password: string
+}
+
+
+const userSchema = new Schema<IItem>({
+    firstname: {
+	type: String,
+	required: true
+    },
+    username: {
+	type: String,
+	required: true,
+	unique: true
+    },
+    email: {
+	type: String,
+	required: true,
+	unique: true
+    },
+    password: {
+	type: String,
+	required: true
+    }
+});
+
+const User : Model<IItem> = mongoose.models.User || mongoose.model<IItem>("Book", userSchema);
+export default User;
