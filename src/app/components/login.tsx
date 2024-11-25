ef4fa38806fc8fcc6f809f64182013ca20817938
Bhargav Gudapati <bhargav_gudapati@yahoo.com>
2024-11-25 09:34:13 -0500

    testing login

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -2,18 +2,25 @@
 'use client';
 import React, { useState } from 'react';
 import styles from '../components/login.module.css';
-import Navbar from './navbar';
 import { useRouter } from 'next/navigation';
+import { signIn, signOut } from "@/auth";
+
+async function doCredentialsLogin(email: string, password: string) {
+    return await signIn("credentials", { email, password, redirect: false });
+}
 
 const LoginPage: React.FC = () => {
 
-    let [user, setUser] = useState("");
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
     const router = useRouter();
     
-    const onLoginSubmit = (e: React.FormEvent) => {
+    const onLoginSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        alert("redirecting to " + user + "\'s page");
         console.log("signing in...");
+	await doCredentialsLogin(email, password);
+	console.log("did the sign in");
+	router.push("/");
     }
 
     const onSignupClick = () => {
@@ -22,14 +29,14 @@ const LoginPage: React.FC = () => {
 
     return (
 	<div>
-	    <Navbar />
-            <div className={styles.loginPage}>            
+            <div className={styles.loginPage}>
+		<p><b>Enter your credentials here:</b></p>
 		<div className={styles.formContainer}>
 		    <form onSubmit={onLoginSubmit}>
-			<label>Username</label>
-			<input value={user} onChange={e => setUser(e.target.value)} type="text" placeholder="Enter username" />                  
+			<label>Email</label>
+			<input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="Enter email" />                  
 			<label>Password</label>
-			<input type="password" placeholder="Enter password" />
+			<input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Enter password" />
 			<button type="submit">Sign In</button>
                     </form>
 		</div>
